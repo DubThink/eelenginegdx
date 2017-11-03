820ec7783bedbfed8237a0a70b2a4a270e695e90
Benjamin Welsh <benjaminwelsh2@gmail.com>
2017-11-03 17:20:31 -0400

    Initial project setup

---
+++ desktop/src/com/eelengine/engine/desktop/DesktopLauncher.java
@@ -0,0 +1,23 @@
+package com.eelengine.engine.desktop;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eelengine.engine.EelGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.useGL30=true;
+		config.foregroundFPS = 288; // 2x 144hz // set to 0 for no frame limit
+        //config.backgroundFPS = 15; // Use 0 to never sleep, -1 to not render.
+        config.vSyncEnabled=true;
+        config.resizable=false;
+        config.title="EelEngine Alpha";
+        config.addIcon("Eel_E_128x.png", Files.FileType.Internal);
+        config.addIcon("Eel_E_32x.png", Files.FileType.Internal);
+        config.addIcon("Eel_E_16x.png", Files.FileType.Internal);
+        //config.stencil=8; // LOOK we can have stencil bits
+		new LwjglApplication(new EelGame(), config);
+	}
+}
