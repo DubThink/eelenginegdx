3c983e2349e50f6bb8ca352e4d7e750de420d916
Benjamin Welsh <benjaminwelsh2@gmail.com>
2018-01-28 16:50:31 -0500

    18: Auto stash before merge of "GGJ18" and "origin/GGJ18"

---
+++ core/src/com/eelengine/engine/CMailbox.java
@@ -0,0 +1,11 @@
+package com.eelengine.engine;
+
+import com.artemis.Component;
+
+/**
+ * A null class to copy.
+ */
+public class CMailbox extends Component {
+    public int mailCt=0;
+    public boolean doneToday=false;
+}
---
+++ core/src/com/eelengine/engine/editor/MailboxSrc.java
@@ -0,0 +1,11 @@
+package com.eelengine.engine.editor;
+
+import com.badlogic.gdx.math.Vector2;
+
+import java.io.Serializable;
+
+public class MailboxSrc implements Serializable{
+    public Vector2 location;
+    public int zone=1;
+
+}
